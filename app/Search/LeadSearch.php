b2fab3851db7e17715e3ab26f2746df6fad0d507
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-30 11:07:48 +0000

    Update LeadSearch.php

--- app/Search/LeadSearch.php
+++ app/Search/LeadSearch.php
@@ -96,6 +96,28 @@ class LeadSearch extends BaseSearch
         return true;
     }
 
+    public function buildReport (Request $request)
+    {
+        $this->query = DB::table('invoices');
+        
+         if(!empty($request->input('group_by')) {
+             // assigned to, status, source_type
+            $this->query->select(DB::raw('count(*) as count, customers.name, task_statuses.name, source_type.name, CONCAT(users.first_name," ",users.last_name) as assigned_to, SUM(valued_at) as valued_at'))
+            $this->query->groupBy($request->input('group_by'));
+        } else {
+            $this->query->select('customers.name, task_statuses.name, source_type.name, valued_at, due_date', DB::raw('CONCAT(first_name," ",last_name) as assigned_to'));
+        }
+
+         $this->query->join('customers', 'customers.id', '=', 'deals.customer_id')
+         ->join('source_type', 'source_type.id', '=', 'deals.source_type')
+         ->join('task_statuses', 'task_statuses.id', '=', 'deals.task_status')
+         ->leftJoin('users', 'users.id', '=', 'deals.assigned_to')
+         ->orderBy('invoices.date_created');
+       
+             //$this->query->where('status', '<>', 1)
+            
+    }
+
     /**
      * @return mixed
      */
