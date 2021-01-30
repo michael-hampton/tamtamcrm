2803ad079d9643428a8cde338ca879eb8d155d93
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-30 11:38:22 +0000

    Update DealSearch.php

--- app/Search/DealSearch.php
+++ app/Search/DealSearch.php
@@ -128,10 +128,10 @@ class DealSearch extends BaseSearch
         
          if(!empty($request->input('group_by')) {
              // assigned to, status, source_type, customer
-            $this->query->select(DB::raw('count(*) as count, customers.name, task_statuses.name, source_type.name, CONCAT(users.first_name," ",users.last_name) as assigned_to, SUM(valued_at) as valued_at'))
+            $this->query->select(DB::raw('count(*) as count, customers.name AS customer, task_statuses.name AS status, source_type.name AS source_type, CONCAT(users.first_name," ",users.last_name) as assigned_to, SUM(valued_at) AS valued_at'))
             $this->query->groupBy($request->input('group_by'));
         } else {
-            $this->query->select('customers.name, task_statuses.name, source_type.name, valued_at, due_date', DB::raw('CONCAT(first_name," ",last_name) as assigned_to'));
+            $this->query->select('customers.name AS customer, task_statuses.name AS status, source_type.name AS source_type, valued_at, due_date', DB::raw('CONCAT(first_name," ",last_name) as assigned_to'));
         }
 
          $this->query->join('customers', 'customers.id', '=', 'deals.customer_id')
