ce2fdacf3d44f84d0c8593a3bb0f12ec5f97f79f
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-30 22:41:04 +0000

    Update DataRow.js

--- resources/assets/js/components/reports/DataRow.js
+++ resources/assets/js/components/reports/DataRow.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import FormatMoney from '../common/FormatMoney'
+import FormatDate from '../common/FormatDate'
 
 class DataRow extends Component {
 
