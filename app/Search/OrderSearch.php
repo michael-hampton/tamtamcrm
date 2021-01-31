2fb3d70dc1bcd798d52277b8ad1dc236ec83fd0f
michael-hampton <michaelhamptondesign@yahoo.com>
2021-01-31 10:46:59 +0000

    Update OrderSearch.php

--- app/Search/OrderSearch.php
+++ app/Search/OrderSearch.php
@@ -147,6 +147,10 @@ class OrderSearch extends BaseSearch
            $this->filterByDate($request->input('date_format'));
         }
 
+        if ($request->input('start_date') <> '' && $request->input('end_date') <> '') {
+            $this->filterDates($request);
+        }
+
         $rows = $this->query->get()->toArray();
 
         if (!empty($request->input('perPage')) && $request->input('perPage') > 0) {
