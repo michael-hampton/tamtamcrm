b7e6faee267967558ca041d2de5de3f4d59badbc
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-02 11:04:00 +0000

    Update Taxable.php

--- app/Traits/Taxable.php
+++ app/Traits/Taxable.php
@@ -1,198 +1,187 @@
 <?php
 trait Taxable {
 
- private function calculateTaxAmount(float $amount, float $rate, bool $useInclusiveTaxes, int $precision) {
+ private function calculateTaxAmount(float $amount, float $rate, bool $useInclusiveTaxes) {
     $taxAmount = 0;
 
     if ($this->uses_inclusive_taxes) {
-      $taxAmount = $amount - ($amount / (1 + ($rate / 100)));
-    } else {
-      $taxAmount = $amount * $rate / 100;
+      return $amount - ($amount / (1 + ($rate / 100)));
     }
-    return round($taxAmount, $precision);
+      
+    return $amount * $rate / 100;
   }
 
-  private function calculateTaxes(bool $useInclusiveTaxes, int $precision) {
-    $total = $this->calculateSubtotal($precision);
+  private function calculateTaxes(bool $useInclusiveTaxes) {
+    $total = $this->calculateSubtotal();
     $taxAmount = 0;
     $map = [];
 
     foreach($this->line_items as $item) {
-      $taxRate1 = round($item->tax_rate, 3);
-      $taxRate2 = round($item->tax_2, 3);
 
-      $lineTotal = $this->getItemTaxable($item, $total, $precision);
+      $lineTotal = $this->getItemTaxable($item, $total);
 
-      if ($taxRate1 > 0) {
+      if ($item->tax_rate > 0) {
         $taxAmount = $this->calculateTaxAmount(
-            $lineTotal, $taxRate1, $this->uses_inclusive_taxes, $precision);
+            $lineTotal, $item->tax_rate, $this->uses_inclusive_taxes);
 
             $map[$item->tax_rate_name] = !isset($map[$item->tax_rate_name]) ? $taxAmount : $map[$item->tax_rate_name] += $taxAmount;
       }
 
-      if ($taxRate2 > 0) {
+      if ($item->tax_2 > 0) {
         $taxAmount = $this->calculateTaxAmount(
-            $lineTotal, $taxRate2, $this->uses_inclusive_taxes, $precision);
-        $map[$item->tax_rate_name] = !isset($map[$item->tax_rate_name]) ? $taxAmount : $map[$item->tax_rate_name] += $taxAmount;
+            $lineTotal, $item->tax_2, $this->uses_inclusive_taxes);
+        $map[$item->tax_rate_name_2] = !isset($map[$item->tax_rate_name_2]) ? $taxAmount : $map[$item->tax_rate_name_2] += $taxAmount;
       }
 
-      if ($taxRate3 != 0) {
+      if ($item->tax_3 != 0) {
         $taxAmount = $this->calculateTaxAmount(
-            $lineTotal, $taxRate3, $this->uses_inclusive_taxes, $precision);
-        $map[$item->tax_rate_name] = !isset($map[$item->tax_rate_name]) ? $taxAmount : $map[$item->tax_rate_name] += $taxAmount;
+            $lineTotal, $item->tax_3, $this->uses_inclusive_taxes);
+        $map[$item->tax_rate_name_3] = !isset($map[$item->tax_rate_name_3]) ? $taxAmount : $map[$item->tax_rate_name_3] += $taxAmount;
       }
     }
 
     if ($this->discount_total > 0) {
       if ($this->is_amount_discount) {
-        $total -= round($this->discount_total, $precision);
+        $total -= $this->discount_total;
       } else {
-        $total -= round($total * $discount_total / 100, $precision);
+        $total -= $total * $discount_total / 100;
       }
     }
 
     if ($this->shipping_cost > 0 && $this->shipping_cost_tax > 0) {
-      $total += round($this->shipping_cost, $precision);
+      $total += $this->shipping_cost;
     }
 
     if ($this->transaction_fee > 0 && $this->transaction_fee_tax > 0) {
-      $total += round($this->transaction_fee, $precision);
+      $total += $this->transaction_fee;
     }
 
     if ($this->tax_rate > 0) {
       $taxAmount =
-          $this->calculateTaxAmount($total, $this->tax_rate, $this->uses_inclusive_taxes, $precision);
+          $this->calculateTaxAmount($total, $this->tax_rate, $this->uses_inclusive_taxes);
       $map[$this->tax_rate_name] = !isset($map[$this->tax_rate_name]) ? $taxAmount : $map[$this->tax_rate_name] += $taxAmount;
     }
 
     if ($this->tax_2 > 0) {
       $taxAmount =
-          $this->calculateTaxAmount($total, $this->tax_2, $this->uses_inclusive_taxes, $precision);
+          $this->calculateTaxAmount($total, $this->tax_2, $this->uses_inclusive_taxes);
       $map[$this->tax_rate_name_2] = !isset($map[$this->tax_rate_name_2]) ? $taxAmount : $map[$this->tax_rate_name_2] += $taxAmount;
     }
 
     if ($this->tax_3 > 0) {
       $taxAmount =
-          $this->calculateTaxAmount($total, $this->tax_3, $this->uses_inclusive_taxes, $precision);
+          $this->calculateTaxAmount($total, $this->tax_3, $this->uses_inclusive_taxes);
       $map[$this->tax_rate_name_3] = !isset($map[$this->tax_rate_name_3]) ? $taxAmount : $map[$this>tax_rate_name_3] += $taxAmount;
     }
 
     return $map;
   }
 
-  private function getItemTaxable($item, float $invoiceTotal, int $precision) {
-    $qty = round($item->quantity, 4);
-    $cost = round($item->cost, 4);
-    $itemDiscount = round($item->discount, $precision);
-    $lineTotal = $qty * $cost;
+  private function getItemTaxable($item, float $invoiceTotal) {
+    $lineTotal = $item->quantity * $item->unit_cost;
 
     if ($this->discount_total > 0) {
       if ($this->is_amount_discount) {
         if ($invoiceTotal > 0) {
-          $lineTotal -= round($lineTotal / $invoiceTotal * $this->discount_total, 4);
+          $lineTotal -= $lineTotal / $invoiceTotal * $this->discount_total;
         }
       }
     }
 
-    if ($itemDiscount > 0) {
+    if ($item->unit_discount > 0) {
       if ($this->is_amount_discount) {
-        $lineTotal -= $itemDiscount;
+        $lineTotal -= $item->unit_discount;
       } else {
-        $lineTotal -= round($lineTotal * $itemDiscount / 100, 4);
+        $lineTotal -= $lineTotal * $itemDiscount / 100;
       }
     }
 
-    return round($lineTotal, $precision);
+    return $lineTotal;
   }
 
-private function calculateTotal(int $precision) {
-    $total = $this->calculateSubtotal($precision);
+private function calculateTotal() {
+    $total = $this->calculateSubtotal();
     $itemTax = 0.0;
 
     foreach($this->line_items as $item) {
-      $qty = round($item->quantity, 4);
-      $cost = round($item->cost, 4);
-      $itemDiscount = round($item->discount, $precision);
       $taxRate1 = round($item->tax_rate, 3);
       $taxRate2 = round($item->tax_2, 3);
-      $lineTotal = $qty * $cost;
+      $lineTotal = $item->quantity * $item->unit_cost;
 
-      if ($itemDiscount != 0) {
+      if ($item->unit_discount > 0) {
         if ($this->is_amount_discount) {
-          $lineTotal -= $itemDiscount;
+          $lineTotal -= $item->unit_discount;
         } else {
-          $lineTotal -= round($lineTotal * $itemDiscount / 100, 4);
+          $lineTotal -= round($lineTotal * $item->unit_discount / 100, 4);
         }
       }
 
       if ($this->discount_total > 0) {
         if ($this->is_amount_discount) {
           if ($this->total != 0) {
-            $lineTotal -= round($lineTotal / $this->total * $this->discount_total, 4);
+            $lineTotal -= $lineTotal / $this->total * $this->discount_total;
           }
         }
       }
       if ($tax_rate > 0) {
-        $itemTax += round($lineTotal * $tax_rate / 100, $precision);
+        $itemTax += $lineTotal * $tax_rate / 100;
       }
       if ($tax_2 > 0) {
-        $itemTax += round($lineTotal * $tax_2 / 100, $precision);
+        $itemTax += $lineTotal * $tax_2 / 100;
       }
     }
 
     if ($this->discount_total > 0) {
       if ($this->is_amount_discount) {
-        $total -= round($this->discount_total, $precision);
+        $total -= $this->discount_total;
       } else {
-        $total -= round($this->total * $this->discount_total / 100, $precision);
+        $total -= $this->total * $this->discount_total / 100;
       }
     }
 
     if ($this->shipping_cost > 0 && $this->shipping_cost_tax > 0) {
-      $total += round($this->shipping_cost, $precision);
+      $total += $this->shipping_cost;
     }
 
     if ($this->transaction_fee > 0 && $this->transaction_fee_tax > 0) {
-      $total += round($this->transaction_fee, $precision);
+      $total += $this->transaction_fee;
     }
 
     if (!$this->uses_inclusive_taxes) {
-      $taxAmount1 = round($total * $this->tax_rate / 100, $precision);
-      $taxAmount2 = round($total * $tax_2 / 100, $precision);
+      $taxAmount1 = $total * $this->tax_rate / 100;
+      $taxAmount2 = $total * $tax_2 / 100;
 
       $total += $itemTax + $taxAmount1 + $taxAmount2;
     }
 
    if ($this->shipping_cost > 0 && empty($this->shipping_cost_tax)) {
-      $total += round($this->shipping_cost, precision);
+      $total += $this->shipping_cost;
     }
 
    if ($this->transaction_fee > 0 && empty($this->transaction_fee_tax)) {
-      $total += round($this->transaction_fee, $precision);
+      $total += $this->transaction_fee;
     }
 
     return $total;
   }
 
-  private function calculateSubtotal(int $precision) {
+  private function calculateSubtotal() {
    $total = 0.0;
 
     foreach($this->line_items as item) {
-      $qty = round(item->quantity, 4);
-      $cost = round(item->cost, 4);
-      $discount = round(item->discount, $precision);
+     
 
-      $lineTotal = $qty * $cost;
+      $lineTotal = $item->quantity * $item->unit_cost;
 
-      if ($discount > 0) {
+      if ($item->unit_discount > 0) {
         if ($this->is_amount_discount) {
-          $lineTotal -= $discount;
+          $lineTotal -= $item->unit_discount;
         } else {
-          $lineTotal -= round($lineTotal * $discount / 100, 4);
+          $lineTotal -= $lineTotal * $discount / 100;
         }
       }
 
-      $total += round($lineTotal, $precision);
+      $total += $lineTotal;
     }
 
     return $total;
