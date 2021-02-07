6ec54a2312b8f6524f18fcfa1a7587e89ce36c88
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-07 14:50:26 +0000

    Update GeneratePdf.php

--- app/Actions/Pdf/GeneratePdf.php
+++ app/Actions/Pdf/GeneratePdf.php
@@ -24,19 +24,6 @@ class GeneratePdf
 
         $label = '';
 
-        switch (strtolower((new \ReflectionClass($this->entity))->getShortName())) {
-            case 'recurringquote':
-                $label = 'quote';
-                break;
-
-            case 'recurringinvoice':
-                $label = 'invoice';
-                break;
-            case 'cases':
-                $label = 'case';
-                break;
-        }
-
         return CreatePdf::dispatchNow(
             (new PdfFactory)->create($this->entity),
             $this->entity,
