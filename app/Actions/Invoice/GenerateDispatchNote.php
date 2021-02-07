f2c578f4437c2bcafcd1204ad256943f1a09a67f
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-07 12:09:39 +0000

    Create GenerateDispatchNote.php

---
+++ app/Actions/Invoice/GenerateDispatchNote.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Actions\Invoice;
+
+
+use App\Models\Invoice;
+use App\Components\Pdf\InvoicePdf;
+
+class GenerateDispatchNote
+{
+    private Invoice $invoice;
+
+    public function __construct(Invoice $invoice)
+    {
+        $this->invoice = $invoice;
+    }
+
+    public function generateDispatchNote($contact = null, $update = false)
+    {
+        if (!$contact) {
+            $contact = $this->entity->customer->primary_contact()->first();
+        }
+
+        $entity = get_class($this->entity) === 'App\\Models\\Order' ? CloneOrderToInvoiceFactory::create(
+            $this->entity,
+            $this->entity->user,
+            $this->entity->account
+        ) : $this->entity;
+
+        return CreatePdf::dispatchNow(
+            (new InvoicePdf($entity)),
+            $this->entity,
+            $contact,
+            $update,
+            'dispatch_note'
+        );
+    }
+}
