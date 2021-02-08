773c93b87ec8fe432b5fba0493559a6e63ec50a7
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-08 20:15:11 +0000

    Update CustomerPresenter.js

--- resources/assets/js/components/presenters/CustomerPresenter.js
+++ resources/assets/js/components/presenters/CustomerPresenter.js
@@ -18,7 +18,37 @@ export function getDefaultTableFields () {
 export default function CustomerPresenter (props) {
     const { field, entity } = props
 
-    switch (field) {
+    switch (field) { 
+        case 'assigned_to': {
+            const assigned_user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.assigned_to))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{assigned_user.length ? `${assigned_user[0].first_name} ${assigned_user[0].last_name}` : ''}</td>
+        }
+        case 'user_id': {
+            const user = JSON.parse(localStorage.getItem('users')).filter(user => user.id === parseInt(props.entity.user_id))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{`${user[0].first_name} ${user[0].last_name}`}</td>
+        }
+        case 'industry_id':
+            const industry = JSON.parse(localStorage.getItem('industries')).filter(user => user.id === parseInt(props.entity.industry_id))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{`${industry[0].name}`}</td>
+        break;
+        case 'language_id':
+            const language = JSON.parse(localStorage.getItem('languages')).filter(user => user.id === parseInt(props.entity.language_id))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{`${language[0].name}`}</td>
+        break;
+        case 'currency_id':
+            const currency = JSON.parse(localStorage.getItem('currencies')).filter(user => user.id === parseInt(props.entity.currency_id))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{`${currency[0].name}`}</td>
+        break;
+        case 'country_id':
+           const country = JSON.parse(localStorage.getItem('countries')).filter(user => user.id === parseInt(props.entity.country_id))
+            return <td onClick={() => props.toggleViewedEntity(entity, entity.number, props.edit)}
+                data-label={field}>{`${country[0].name}`}</td>
+        break;
         case 'id':
             return <Avatar name={entity.name}/>
         case 'date':
