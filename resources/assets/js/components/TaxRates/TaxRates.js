8aae1e27e6c076d6b2eb0a790a09a5d1c310cbb7
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-09 23:53:57 +0000

    Update TaxRates.js

--- resources/assets/js/components/TaxRates/TaxRates.js
+++ resources/assets/js/components/TaxRates/TaxRates.js
@@ -14,6 +14,10 @@ export default class TaxRates extends Component {
         super(props)
 
         this.state = {
+            currentPage: 1,
+             totalPages: null,
+             pageLimit: !localStorage.getItem('number_of_rows') ? Math.ceil(window.innerHeight / 90) : localStorage.getItem('number_of_rows'),
+             currentInvoices: [],
             isOpen: window.innerWidth > 670,
             taxRates: [],
             cachedData: [],
