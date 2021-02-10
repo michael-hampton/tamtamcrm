b74ce7b5a08af22f8d9c0f173ab7c9dcaeb9fb1e
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-10 00:55:57 +0000

    Update RecurringInvoices.js

--- resources/assets/js/components/recurringInvoices/RecurringInvoices.js
+++ resources/assets/js/components/recurringInvoices/RecurringInvoices.js
@@ -178,7 +178,7 @@ export default class RecurringInvoices extends Component {
     }
 
     render () {
-        const { invoices, custom_fields, customers, allInvoices, view, filters, error, isOpen, error_message, success_message, show_success } = this.state
+        const { invoices, custom_fields, customers, allInvoices, view, filters, error, isOpen, error_message, success_message, show_success, currentInvoices, currentPage, totalPages, pageLimit } = this.state
         const { status_id, customer_id, searchText, start_date, end_date, project_id, user_id } = this.state.filters
         const fetchUrl = `/api/recurring-invoice?search_term=${searchText}&user_id=${user_id}&status=${status_id}&customer_id=${customer_id}&project_id=${project_id}&start_date=${start_date}&end_date=${end_date}`
         const addButton = customers.length && allInvoices.length
