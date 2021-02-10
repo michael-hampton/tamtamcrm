4a4505257ab18fb13f678d37bd735972411320ba
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-10 00:40:49 +0000

    Update TaxRateFilters.js

--- resources/assets/js/components/TaxRates/TaxRateFilters.js
+++ resources/assets/js/components/TaxRates/TaxRateFilters.js
@@ -67,10 +67,9 @@ export default class TaxRateFilters extends Component {
                     <TableSearch onChange={(e) => {
                         const value = typeof e.target.value === 'string' ? e.target.value.toLowerCase() : e.target.value
                         const search_results = this.props.cachedData.filter(obj => Object.keys(obj).some(key => obj[key] && obj[key].length ? obj[key].toString().toLowerCase().includes(value) : false))
-                        if (search_results && search_results.length) {
-                            const totalPages = Math.ceil(search_results / this.props.pageLimit);
-                            this.props.updateList({ invoices: search_results, currentPage: 1, totalPages: totalPages })
-                        }
+                        const totalPages = Math.ceil(search_results && search_results.length ? (search_results / this.props.pageLimit) : 0);
+                        this.props.updateList({ invoices: search_results && search_results.length ? search_results : [], currentPage: 1, totalPages: totalPages })
+                        
                     }}/>
                 </Col>
 
