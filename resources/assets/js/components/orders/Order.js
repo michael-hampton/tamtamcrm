87a82e4fd78d99339c79f4b2b900547a35a2460d
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-10 00:21:58 +0000

    Update Order.js

--- resources/assets/js/components/orders/Order.js
+++ resources/assets/js/components/orders/Order.js
@@ -215,6 +215,12 @@ export default class Order extends Component {
                         <Card>
                             <CardBody>
                                 <DataTable
+
+pageLimit={pageLimit}
+                                     onPageChanged={this.onPageChanged.bind(this)}
+                                     currentData={currentInvoices}
+                                     hide_pagination={true}
+
                                     default_columns={getDefaultTableFields()}
                                     setSuccess={this.setSuccess.bind(this)}
                                     setError={this.setError.bind(this)}
