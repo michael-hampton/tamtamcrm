bbbdc7b2658313b50458fbdd11a0e726dbc33071
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-10 00:50:14 +0000

    Update Customers.js

--- resources/assets/js/components/customers/Customers.js
+++ resources/assets/js/components/customers/Customers.js
@@ -146,7 +146,7 @@ export default class Customers extends Component {
 
     render () {
         const { searchText, status, company_id, group_settings_id, start_date, end_date } = this.state.filters
-        const { custom_fields, customers, companies, error, view, filters, isOpen, error_message, success_message, show_success } = this.state
+        const { custom_fields, customers, companies, error, view, filters, isOpen, error_message, success_message, show_success, currentInvoices, currentPage, totalPages, pageLimit } = this.state
         const fetchUrl = `/api/customers?search_term=${searchText}&status=${status}&company_id=${company_id}&group_settings_id=${group_settings_id}&start_date=${start_date}&end_date=${end_date}`
         const addButton = companies.length ? <AddCustomer
             custom_fields={custom_fields}
