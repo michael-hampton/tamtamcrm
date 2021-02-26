6f61c0310cacd09cf74fda70101c7cf619b5eb7f
michael-hampton <michaelhamptondesign@yahoo.com>
2021-02-26 20:24:12 +0000

    Update ResetPasswordController.php

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,39 +1,95 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
-
+namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
-class ResetPasswordController extends Controller
+class ResetPasswordAPIController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
 
     use ResetsPasswords;
 
     /**
-     * Where to redirect users after resetting their password.
+     * Reset the given user's password.
      *
-     * @var string
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
-    protected $redirectTo = '/home';
+    public function reset(Request $request)
+    {
+        //$request->validate($this->rules(), $this->validationErrorMessages());
 
+        $validator = Validator::make($request->all(), [
+            'token' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255' ],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        return $response == Password::PASSWORD_RESET
+                    ? $this->sendResetResponse($request, $response)
+                    : $this->sendResetFailedResponse($request, $response);
+    }
     /**
-     * Create a new controller instance.
+     * Reset the given user's password.
      *
+     * @param  \Illuminate\Contracts\Auth\CanResetPassword  $user
+     * @param  string  $password
      * @return void
      */
-    public function __construct()
+    protected function resetPassword($user, $password) 
+    { 
+        $this->setUserPassword($user, $password); 
+        //Here Larvel tries to set the "Remember me" cookie 
+        //$user->setRememberToken(Str::random(60)); 
+
+        $user->save(); 
+        event(new PasswordReset($user)); 
+        //By default, Laravel will attempt to automatically log in the user 
+        //$this->guard()->login($user); 
+    }
+
+    /**
+     * Get the response for a successful password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function sendResetResponse(Request $request, $response)
     {
-        $this->middleware('guest');
+        return response()->json(['success' => ["message" => trans($response)] ], 200);                          
+    }
+
+    /**
+     * Get the response for a failed password reset.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        // return redirect()->back()
+        //             ->withInput($request->only('email'))
+        //             ->withErrors(['email' => trans($response)]);
+        return response()->json(['error' => ["message" => trans($response)] ], 422);            
     }
 }
