9e4b5f1c50f26d3be376c9a71981d0d91033e1e3
michael-hampton <michaelhamptondesign@yahoo.com>
2021-03-13 14:48:15 +0000

    Update RecurringInvoiceController.php

--- app/Http/Controllers/RecurringInvoiceController.php
+++ app/Http/Controllers/RecurringInvoiceController.php
@@ -90,10 +90,8 @@ class RecurringInvoiceController extends BaseController
      * @param CreateRecurringInvoiceRequest $request
      * @return mixed
      */
-    public function update(int $id, CreateRecurringInvoiceRequest $request)
+    public function update(CreateRecurringInvoiceRequest $request, RecurringInvoice $recurring_invoice)
     {
-        $recurring_invoice = $this->recurring_invoice_repo->findInvoiceById($id);
-
         $recurring_invoice = $this->recurring_invoice_repo->save($request->all(), $recurring_invoice);
         return response()->json($this->transformRecurringInvoice($recurring_invoice));
     }
@@ -115,16 +113,14 @@ class RecurringInvoiceController extends BaseController
      * @param int $id
      * @return mixed
      */
-    public function archive(int $id)
+    public function archive(RecurringInvoice $recurring_invoice)
     {
-        $recurring_invoice = $this->recurring_invoice_repo->findInvoiceById($id);
         $recurring_invoice->archive();
         return response()->json([], 200);
     }
 
-    public function destroy(int $id)
+    public function destroy(RecurringInvoice $recurring_invoice)
     {
-        $recurring_invoice = RecurringInvoice::withTrashed()->where('id', '=', $id)->first();
         $this->authorize('delete', $recurring_invoice);
         $recurring_invoice->deleteEntity();
         return response()->json([], 200);
