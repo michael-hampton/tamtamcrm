d973ba5088254cffa16625e5b08dce7f3ce16921
michael-hampton <michaelhamptondesign@yahoo.com>
2021-03-13 14:50:09 +0000

    Update RecurringQuoteController.php

--- app/Http/Controllers/RecurringQuoteController.php
+++ app/Http/Controllers/RecurringQuoteController.php
@@ -92,10 +92,8 @@ class RecurringQuoteController extends BaseController
      * @param UpdateRecurringQuoteRequest $request
      * @return JsonResponse
      */
-    public function update(int $id, UpdateRecurringQuoteRequest $request)
+    public function update(UpdateRecurringQuoteRequest $request, RecurringQuote $recurring_quote)
     {
-        $recurring_quote = $this->recurring_quote_repo->findQuoteById($id);
-
         $recurring_quote = $this->recurring_quote_repo->save($request->all(), $recurring_quote);
         return response()->json($this->transformRecurringQuote($recurring_quote));
     }
@@ -117,9 +115,8 @@ class RecurringQuoteController extends BaseController
      * @param int $id
      * @return mixed
      */
-    public function archive(int $id)
+    public function archive(RecurringQuote $recurring_quote)
     {
-        $recurring_quote = $this->recurring_quote_repo->findQuoteById($id);
         $recurring_quote->archive();
         return response()->json([], 200);
     }
@@ -129,9 +126,8 @@ class RecurringQuoteController extends BaseController
      * @return mixed
      * @throws AuthorizationException
      */
-    public function destroy(int $id)
+    public function destroy(RecurringQuote $recurring_quote)
     {
-        $recurring_quote = RecurringQuote::withTrashed()->where('id', '=', $id)->first();
         $this->authorize('delete', $recurring_quote);
         $recurring_quote->deleteEntity();
         return response()->json([], 200);
