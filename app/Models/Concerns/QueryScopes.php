5f629464a62778b6a62c4a1c1592f61b6212c8a3
michael-hampton <michaelhamptondesign@yahoo.com>
2021-04-25 19:03:47 +0100

    Update QueryScopes.php

--- app/Models/Concerns/QueryScopes.php
+++ app/Models/Concerns/QueryScopes.php
@@ -1,11 +1,36 @@
+<?php
 namespace App\Models\Concerns;
 
 
 use App\Models\Customer;
+use App\Models\Account;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 trait QueryScopes
 {
+    public function scopeByDate($query, $date_from, $date_to)
+    {
+        return $query->where('plan_id', $planId);
+    }
 
+    public function scopeByCustomer($query, int $customer_id)
+    {
+        return $query->where('customer_id', $customer_id);
+    }
+
+    public function scopeByAccount($query, Account $account)
+    {
+        return $query->where('account_id', $account->id);
+    }
+
+    public function scopeByProject($query, int $project_id)
+    {
+        return $query->where('project_id', $project_id);
+    }
+
+    public function scopeById($query, int $id)
+    {
+        return $query->where('id', $id);
+    }
 }
