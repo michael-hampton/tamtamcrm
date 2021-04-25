cf93aa51907abc5838605a9c3ab8b232ad915a27
michael-hampton <michaelhamptondesign@yahoo.com>
2021-04-25 18:57:46 +0100

    Create QueryScopes.php

---
+++ app/Models/Concerns/QueryScopes.php
@@ -0,0 +1,11 @@
+namespace App\Models\Concerns;
+
+
+use App\Models\Customer;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+trait QueryScopes
+{
+
+}
